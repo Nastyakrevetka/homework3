62990f849d7694d035eb459b11a2c9e8687ed9be
Nastya_krevtka <nastyamkala1278@gmail.com>
2022-12-19 02:42:27 +0300

    задача 1

---
+++ ex1/Program.cs
@@ -0,0 +1,20 @@
+﻿int Palindrom (string str)
+{
+Console.Write(str);
+int numb = int.Parse(Console.ReadLine());
+int revers =0;
+while (numb>0)
+{
+    revers= revers*10+ numb%10;
+    numb= numb/10;
+}
+return revers;
+}
+Console.WriteLine("Введите число: ");
+int Numb = int.Parse(Console.ReadLine());
+int ReversNumb= Palindrom("введите число повторно, пожалуйста: ");
+Console.WriteLine(ReversNumb);
+if (Numb == ReversNumb)
+{Console.Write("число палиндром");}
+else 
+{Console.Write("число не полиндром");}
