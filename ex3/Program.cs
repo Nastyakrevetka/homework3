7ddaa000145025c6e5dc9ff538f4e2f3e4f0fc1f
Nastya_krevtka <nastyamkala1278@gmail.com>
2022-12-19 02:55:12 +0300

    задача 3

---
+++ ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int FindCube(string str)
+{
+Console.Write(str);
+int N = int.Parse(Console.ReadLine());
+int min=1;
+while (min<(N+1))
+{
+    int sqr= min*min*min;
+    Console.WriteLine(sqr);
+    min++;
+}
+return N;
+}
+int N= FindCube("Введите N: ");
